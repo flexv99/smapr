f3e678713c915662d303be15d1fb6af72e818d8a
flex99 <flex99@posteo.net>
2024-03-20 00:24:37 +0100

    color changes

--- src/Renderer/Lines.hs
+++ src/Renderer/Lines.hs
@@ -20,12 +20,11 @@ contents =
   , D_      <<- ( mA 0 340 <> lA 113 170 <> lA 0 0 <> lA 85 0
                   <> lA 198 170 <> lA 85 340 <> lA 0 340 <> z <> mA 0 340 ) ]
 
-
 renderCommands :: [Geometry] -> Element
 renderCommands g =
   path_ [
   Fill_     <<- "None"
-  , Stroke_ <<- "#352950"
+  , Stroke_ <<- "#ffffff"
   , D_      <<- ( T.intercalate " " $ map (geoToSvgPath) g )
         ]
 
