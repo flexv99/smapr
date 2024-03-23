3d51ff6e8135fe126b4b270a986d5a35119a271c
flex99 <flex99@posteo.net>
2024-03-23 01:18:29 +0100

    minor cleanup

--- src/ApiClient.hs
+++ src/ApiClient.hs
@@ -23,29 +23,6 @@ data Coord = Coord
   , zoom :: Double
   } deriving Show
 
-newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
-
-instance Monad m => Monad (MaybeT m) where
-  return  = MaybeT . return . Just
-  x >>= f = MaybeT $ do val <- runMaybeT x
-                        case val of
-                          Just val -> runMaybeT $ f val
-                          Nothing  -> return Nothing
-
-instance Monad m => Applicative (MaybeT m) where
-    pure = return
-    (<*>) = ap
-
-instance Monad m => Functor (MaybeT m) where
-    fmap = liftM
-
-instance Monad m => Alternative (MaybeT m) where
-    empty   = MaybeT $ return Nothing
-    x <|> y = MaybeT $ do maybe_value <- runMaybeT x
-                          case maybe_value of
-                               Nothing    -> runMaybeT y
-                               Just _     -> return maybe_value
-
 host :: IO (String)
 host = smaprConfig >>= return . baseUrl
 
