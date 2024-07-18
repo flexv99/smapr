ba2d6305bbdd498c524b74b9c6cd7c55056e6b82
flex99 <flex99@posteo.net>
2024-07-18 09:39:23 +0200

    remapped unit tests

--- test/Decoder/DecoderSpec.hs
+++ test/Decoder/DecoderSpec.hs
@@ -1,4 +1,4 @@
-module Decoder.DecoderSpec where
+module Decoder.DecoderSpec (spec) where
 
 import qualified Data.Sequence as S
 import qualified Data.Map as MP
@@ -11,12 +11,11 @@ import ApiClient
 waterLayer :: IO (Maybe Layer)
 waterLayer = fakerTile <&> fmap (\l -> getLayers "water" l `S.index` 0)
 
-decodeSpec :: Spec
-decodeSpec = do
+spec :: Spec
+spec = do
   describe "key value mapper" $ do
     it "map a feature's tags to the according keys and values" $ do
-      let app = waterLayer <&> fmap featureProperties
-      app `shouldReturn` featureTagsMapperRes
+      (waterLayer <&> fmap featureProperties) `shouldReturn` featureTagsMapperRes
 
 featureTagsMapperRes :: Maybe [MP.Map String String]
 featureTagsMapperRes = Just [MP.fromList [("collision_rank","river"),("id","41431201"),("kind","stream"),("min_zoom","11.0"),("name","512"),("name:de","Gran Ega"),("name:it","Gader Bach"),("sort_rank","201"),("source","openstreetmap.org")],MP.fromList [("collision_rank","river"),("id","41431201"),("kind","stream"),("min_zoom","11.0"),("name","512"),("name:de","Gran Ega"),("name:it","Gader Bach"),("sort_rank","201"),("source","openstreetmap.org")],MP.fromList [("collision_rank","river"),("id","41431201"),("kind","stream"),("min_zoom","11.0"),("name","512"),("name:de","Gran Ega"),("name:it","Gader Bach"),("sort_rank","201"),("source","openstreetmap.org")]]
