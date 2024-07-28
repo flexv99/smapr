b26c297a1198509c8b1bbaadef9af548f5cb0a4e
flex99 <flex99@posteo.net>
2024-07-28 11:44:12 +0200

    added negation wrapper and in filter expression parser

--- lib/Style/FilterExpressions.hs
+++ lib/Style/FilterExpressions.hs
@@ -3,6 +3,7 @@
 module Style.FilterExpressions where
 
 import Data.Kind (Type)
+import Data.Functor ((<&>))
 import Proto.Vector_tile.Tile.Feature (Feature(..))
 import Proto.Vector_tile.Tile.Layer (Layer(..))
 import qualified Data.Text.Lazy as T
@@ -14,6 +15,7 @@ import Text.Megaparsec.Char
 import Style.Parser
 import Style.Expressions
 import Proto.Util
+import ApiClient
 
 data FilterBy
   = FTypeOf
@@ -23,30 +25,64 @@ data FilterBy
 
 -- | AST representation of filter expressions
 data FilterExpr :: SType -> Type where
-  -- | filter equality
-  FeqE :: FilterBy -> SType -> FilterExpr (SBool b)
+  StringFe :: T.Text -> FilterExpr (SString s)
+  ArrayFe  :: SType -> FilterExpr (SArray a)
+  Negation :: FilterExpr (SBool s) -> FilterExpr (SBool b)
+  -- | filter by equality
+  FeqE  :: FilterBy -> SType -> FilterExpr (SBool b)
+  -- | in lookup
+  FinE  :: FilterBy -> SType -> FilterExpr (SBool b)
 
 deriving instance Show (FilterExpr res)
+
+typeParser :: Parser T.Text
+typeParser = label "type" $ betweenDoubleQuotes $ do
+  _ <- char '$'
+  string "type"
 
 -- | choice of possible filtering types:
 -- id, type, feature properties
 filterByP :: Parser FilterBy
 filterByP = choice [ FId <$> pInteger
-                   , FTypeOf <$ string "$type"
+                   , try $ FTypeOf <$ typeParser
                    , FProp <$> pString
                    ]
 
-feqP :: Parser (FilterExpr ('SBool b))
-feqP = exprBaseP "==" $ do
+fEqP :: Parser (FilterExpr ('SBool b))
+fEqP = betweenSquareBrackets $ do
+  key <- betweenDoubleQuotes (string "!=" <|> string "==")
+  _ <- char ',' >> space
   val1 <- filterByP
   _ <- char ',' >> space
-  FeqE val1 <$> stringLitP
+  val2 <- stringLitP
+  let expr = FeqE val1 val2
+  if T.isPrefixOf "!" key then return $ Negation expr else return expr
+
+fInP :: Parser (FilterExpr ('SBool b))
+fInP = betweenSquareBrackets $ do
+  key <- betweenDoubleQuotes (string "!in" <|> string "in")
+  _ <- char ',' >> space
+  val <- filterByP
+  _ <- char ',' >> space
+  props <- SArray <$> (stringLitP `sepBy` (char ',' >> space))
+  let expr = FinE val props
+  if T.isPrefixOf "!" key then return $ Negation expr else return expr
 
 
 evalFilterExpr :: FilterExpr a -> Feature -> Layer -> Bool
-evalFilterExpr (FeqE a b) = evalFilterEq a b
+evalFilterExpr (Negation e) f l = not $ evalFilterExpr e f l
+evalFilterExpr (FeqE a b)   f l = evalFilterEq a b f l
+evalFilterExpr (FinE a b)   f l = evalFilterIn a b f l
 
-evalFilterEq :: FilterBy ->  SType -> Feature -> Layer -> Bool
-evalFilterEq FTypeOf (SString s) f l     = (Just s ==) $ geometryTypeToString f
+evalFilterEq :: FilterBy -> SType -> Feature -> Layer -> Bool
+evalFilterEq FTypeOf (SString s) f l     = (Just (T.toCaseFold s) ==) $ T.toCaseFold <$> geometryTypeToString f
 evalFilterEq (FId id) (SString s) f l    = (Just s ==) $ featureIdToString f
-evalFilterEq (FProp key) (SString s) f l = undefined
+evalFilterEq (FProp key) (SString s) f l = (Just s ==) $ key `MP.lookup` featureProperties l f
+evalFilterEq _ _ f l                     = error "wrong params"
+
+evalFilterIn :: FilterBy -> SType -> Feature -> Layer -> Bool
+evalFilterIn = undefined
+
+waterLayer :: IO (Maybe Layer)
+waterLayer = fakerTile <&> fmap (\l -> getLayers "water" l `S.index` 0)
+ 
