265a718fa00d449fe9d38e115e08271467bb6713
flex99 <flex99@flex99.flex99>
2024-09-12 10:52:07 +0100

    additional helpers tying to figure out atop stacking of the layers

--- lib/Style/Poc.hs
+++ lib/Style/Poc.hs
@@ -1,45 +1,45 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Style.Poc where
 
+import ApiClient
+import Control.Monad
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy.Internal as B
-import qualified Diagrams.Prelude as D
-import qualified Diagrams.Backend.SVG as D
-import qualified Data.Sequence as S
-import Data.Foldable
-import Control.Monad
-import Util
-import Text.Megaparsec
 import Data.Colour.SRGB
-import GHC.Generics
+import Data.Foldable
 import Data.Maybe
-import Style.Parser
-import Style.ExpressionsWrapper
-import Style.ExpressionsEval
-import Style.IsoExpressions
-import Style.FeatureExpressions
-import Style.ExpressionsContext
-import ApiClient
+import qualified Data.Sequence as S
+import qualified Diagrams.Backend.SVG as D
+import qualified Diagrams.Prelude as D
+import GHC.Generics
+import Proto.Util
 import Proto.Vector_tile.Tile
 import Proto.Vector_tile.Tile.Layer
-import Proto.Util
 import Renderer.Geometry
+import Style.ExpressionsContext
+import Style.ExpressionsEval
+import Style.ExpressionsWrapper
+import Style.FeatureExpressions
+import Style.IsoExpressions
 import Style.Layers.Wrapper
-
+import Style.Parser
+import Text.Megaparsec
+import Util
 
 -- The goal of this proof of concept is to correctly parse the style of this water way
 -- and apply this style to my test vector tile unsing Render.Geomety.renderLayer.
 
 data Width = Width
-  { base :: Maybe SType
-  , stops :: Maybe SType
-  } deriving (Show, Eq, Generic)
+  { base :: Maybe SType,
+    stops :: Maybe SType
+  }
+  deriving (Show, Eq, Generic)
 
 -- Helper for use in combination with .:? to provide default values for optional JSON object fields.
 
@@ -49,10 +49,11 @@ instance A.FromJSON Width where
       <$> obj A..:? "base"
       <*> obj A..:? "stops"
 
-
-
 waterLayerStyle :: B.ByteString
 waterLayerStyle = "{\"id\":\"waterway\",\"type\":\"line\",\"source\":\"openmaptiles\",\"source-layer\":\"waterway\",\"filter\":[\"all\",[\"==\",[\"geometry-type\"],\"LineString\"],[\"match\",[\"get\",\"brunnel\"],[\"bridge\",\"tunnel\"],false,true],[\"!=\",[\"get\",\"intermittent\"],1]],\"layout\":{\"visibility\":\"visible\"},\"paint\":{\"line-color\":\"hsl(205,56%,73%)\",\"line-opacity\":1,\"line-width\":[\"interpolate\",[\"exponential\",1.4],[\"zoom\"],8,1,20,8]}}"
+
+waterFill :: B.ByteString
+waterFill = "{\"id\":\"water\",\"type\":\"fill\",\"source\":\"openmaptiles\",\"source-layer\":\"water\",\"filter\":[\"all\",[\"==\",[\"geometry-type\"],\"Polygon\"],[\"!=\",[\"get\",\"intermittent\"],1],[\"!=\",[\"get\",\"brunnel\"],\"tunnel\"]],\"layout\":{\"visibility\":\"visible\"},\"paint\":{\"fill-color\":\"hsl(205,56%,73%)\"}}"
 
 transportationLayerStyle :: B.ByteString
 transportationLayerStyle = "{\"id\":\"road_trunk_primary\",\"type\":\"line\",\"source\":\"openmaptiles\",\"source-layer\":\"transportation\",\"filter\":[\"all\",[\"==\",[\"geometry-type\"],\"LineString\"],[\"match\",[\"get\",\"class\"],[\"primary\",\"trunk\"],true,false]],\"layout\":{\"line-cap\":\"round\",\"line-join\":\"round\"},\"paint\":{\"line-color\":\"#fff\",\"line-width\":[\"interpolate\",[\"exponential\",1.4],[\"zoom\"],6,0.5,20,30]}}"
@@ -69,9 +70,8 @@ l2 = "{\"id\":\"landcover_grass\",\"type\":\"fill\",\"source\":\"openmaptiles\",
 l3 :: B.ByteString
 l3 = "{\"id\":\"landcover_wood\",\"type\":\"fill\",\"source\":\"openmaptiles\",\"source-layer\":\"landcover\",\"filter\":[\"==\",[\"get\",\"class\"],\"wood\"],\"paint\":{\"fill-color\":\"hsl(82,46%,72%)\",\"fill-opacity\":[\"interpolate\",[\"linear\"],[\"zoom\"],8,0.6,22,1]}}"
 
-
 testLayers :: [B.ByteString]
-testLayers = [waterLayerStyle, transportationLayerStyle, l1, l2, l3, buildingsLayerStyle]
+testLayers = [waterLayerStyle, waterFill, transportationLayerStyle, buildingsLayerStyle, l1, l2, l3]
 
 testEval :: String -> WrappedExpr -> Tile -> [SType]
 testEval layer expr t = map (eval expr) ctxs
@@ -81,14 +81,19 @@ testEval layer expr t = map (eval expr) ctxs
 
 renderStyles :: B.ByteString -> Tile -> Maybe (D.Diagram D.B)
 renderStyles sts' t =
-  let stile   = A.decode sts' :: Maybe SLayer
-      tbD     = toBeDrawn t <$> stile
-  in (renderLayer . paint <$> stile) <*> tbD
+  let stile = A.decode sts' :: Maybe SLayer
+      tbD = toBeDrawn t <$> stile
+   in (renderLayer . paint <$> stile) <*> tbD
+
+buildFinalDiagram :: Tile -> D.Diagram D.B
+buildFinalDiagram t = D.bg (sRGB24 232 229 216) (foldl D.atop (D.strutX 0) (mapMaybe (`renderStyles` t) testLayers))
 
 test :: IO ()
 test = do
-  t <- getMTTile (Coord 46.611106 11.895304 14)
-  let renderedLayers = catMaybes $ mapMaybe (\x -> renderStyles x <$> t) testLayers
-  let diagram = D.bg (sRGB24 232 229 216) (foldl1 D.atop renderedLayers)
-  writeSvg diagram
-  -- maybe (putStrLn "Nothing") writeSvg diagram
+  t <- getMTTile (Coord 46.785019 11.931154 14)
+  maybe (putStrLn "Noting") (writeSvg . buildFinalDiagram) t
+
+testWithUrl :: String -> IO ()
+testWithUrl url = do
+  t <- getFromUrl url
+  maybe (putStrLn "Noting") (writeSvg . buildFinalDiagram) t
