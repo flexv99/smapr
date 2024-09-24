93b6e05d0d4d8b2a037150311d80486b93b2f703
flex99 <flex99@flex99.flex99>
2024-09-24 08:59:52 +0100

    added missing background layer

---
+++ lib/Style/Layers/Background.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Style.Layers.Background where
+
+import Control.Lens
+import qualified Data.Aeson as A
+import Data.Colour
+import Style.ExpressionsWrapper
+import Style.Layers.Util
+import Style.Parser
+
+data BackgroundS = BackgroundS
+  { _visibility :: Visibility, -- defaults to visible
+    _backgroundOpacity :: IsoExpr INum, -- defaults to 1, need to support inrepolate expressions
+    _backgroundColor :: IsoExpr Color -- defaults to #000000, disabled by fill-pattern
+    -- , _backgroundPattern
+  }
+  deriving (Show)
+
+makeLenses ''BackgroundS
+
+instance A.FromJSON BackgroundS where
+  parseJSON = A.withObject "BackgroundS" $ \t ->
+    BackgroundS
+      <$> t A..:? "visibility" A..!= Visible
+      <*> (t A..:? "background-opacity" >>= expr) A..!= NumE 1
+      <*> (t A..:? "background-color" >>= color) A..!= ColorE (black `withOpacity` 1)
+
+-- background-pattern
